947df1e137970a096dd7fce3ad7f92601e1cb002
mungai <mungaihosea@gmail.com>
2020-02-19 14:57:49 -0500

    blog

--- blog/models.py
+++ blog/models.py
@@ -3,12 +3,15 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.TextField()
-    image1 = models.ImageField(null = True)
-    image2 = models.ImageField(null = True)
-    image3 = models.ImageField(null = True)
+    image1 = models.ImageField(blank = True, null = True)
+    image2 = models.ImageField(blank = True, null = True)
+    image3 = models.ImageField(blank = True, null = True)
     content = models.TextField()
     date = models.DateTimeField(auto_now_add = True)
 
+    def __str__(self):
+        return self.title
+        
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete = models.CASCADE)
     content = models.TextField()
